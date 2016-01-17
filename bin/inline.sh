9ec3cd00f483d533ea5c485877d35c624bf42694
crvv <crvv.pku@gmail.com>
2016-01-17 05:30:42 +0000

    fix error and reformat code

--- bin/inline.sh
+++ bin/inline.sh
@@ -12,85 +12,92 @@ VERSION="0.1"
 FILES=()
 
 error_exit() {
-  echo -e "${PROGNAME}: ${1:-"Unknown Error"}" >&2
-  exit 1
+	echo -e "${PROGNAME}: ${1:-"Unknown Error"}" >&2
+	exit 1
 }
 
 graceful_exit() {
-    exit
+	exit
 }
 
 usage() {
-    echo -e "Usage: $PROGNAME [-h|--help] file.conf"
+	echo -e "Usage: $PROGNAME [-h|--help] file.conf"
 }
 
 help_message() {
-  cat <<- _EOF_
-  $PROGNAME ver. $VERSION
-  Replace include statements with the included files
+	cat <<- _EOF_
+	$PROGNAME ver. $VERSION
+	Replace include statements with the included files
 
-  The primary use of this script is for debugging nginx
-  config files, or to provide a single config file for
-  distribution/deployment.
+	The primary use of this script is for debugging nginx
+	config files, or to provide a single config file for
+	distribution/deployment.
 
-  $(usage)
+	$(usage)
 
-  Example:
-    cd /etc/nginx
-    $PROGNAME h5bp/basic.conf
-    ### h5bp/basic.conf START
+	Example:
+	cd /etc/nginx
+	$PROGNAME h5bp/basic.conf
+	### h5bp/basic.conf START
 
-    ### h5bp/directive-only/x-ua-compatible.conf START
-    add_header "X-UA-Compatible" "IE=Edge";
-    ....
+	### h5bp/directive-only/x-ua-compatible.conf START
+	add_header "X-UA-Compatible" "IE=Edge";
+	....
 
-  Options:
-  -h, --help  Display this help message and exit.
+	Options:
+	-h, --help  Display this help message and exit.
 
-_EOF_
-  return
+	_EOF_
+	return
 }
 
+function output {
+while read -r line
+do
+	if [[ $line =~ "#" ]];
+	then
+		# skip comments
+		continue
+	elif [[ $line =~ "^ *$" ]];
+	then
+		# skip blank lines
+		continue
+	elif [[ $line =~ ^(\s*)include\s*(.*)\; ]];
+	then
+		main ${BASH_REMATCH[2]} 
+	else
+		echo "$line"
+	fi
+done < $1
+}
 function main {
-    echo "### $1 START"
-    while read -r line
-    do
-        if [[ $line =~ "#" ]];
-        then
-            # skip comments
-            continue
-        elif [[ $line =~ "^ *$" ]];
-        then
-            # skip blank lines
-            continue
-        elif [[ $line =~ ^(\s*)include\s*(.*)\; ]];
-        then
-            main ${BASH_REMATCH[2]}
-        else
-            echo "$line"
-        fi
-    done < $1
-    echo "### $1 END"
+for i in $@
+do
+    echo $i 1>&2
+	echo "### $i START"
+	output $i
+	echo "### $i END"
+done
 }
 
 # Parse command-line
 while [[ -n $1 ]]; do
-  case $1 in
-    -h | --help)
-      help_message; graceful_exit ;;
-    -* | --*)
-      usage
-      error_exit "Unknown option $1" ;;
-    *)
-      FILES+=($1);;
-  esac
-  shift
+	case $1 in
+		-h | --help)
+			help_message; graceful_exit ;;
+		-* | --*)
+			usage
+			error_exit "Unknown option $1" ;;
+		*)
+			FILES+=($1);;
+	esac
+	shift
 done
 
 if [ ${#FILES[@]} -eq 0 ];
 then
-      usage;
-      graceful_exit;
+	usage;
+	graceful_exit;
 fi
 
 main $FILES
